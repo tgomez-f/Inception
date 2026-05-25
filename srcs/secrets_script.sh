97397c38514ee9e828870ec8a663a1f18c31c3e5
tgomez-f <tgomez-f@debian-Inception.42.fr>
2026-05-25 18:02:06 +0200

    fix: only generate password one time

--- srcs/secrets_script.sh
+++ srcs/secrets_script.sh
@@ -2,10 +2,11 @@
 
 mkdir -p "secrets"
 
-openssl rand -base64 15 > "secrets/db_password.txt"
-openssl rand -base64 15 > "secrets/db_root_password.txt"
-openssl rand -base64 15 > "secrets/wp_admin_password.txt"
-openssl rand -base64 15 > "secrets/wp_user_password.txt"
+[ ! -f "secrets/db_password.txt" ]      && openssl rand -base64 15 > "secrets/db_password.txt"
+[ ! -f "secrets/db_root_password.txt" ] && openssl rand -base64 15 > "secrets/db_root_password.txt"
+[ ! -f "secrets/wp_admin_password.txt" ] && openssl rand -base64 15 > "secrets/wp_admin_password.txt"
+[ ! -f "secrets/wp_user_password.txt" ] && openssl rand -base64 15 > "secrets/wp_user_password.txt"
+
 
 echo "Secrets added."
 
